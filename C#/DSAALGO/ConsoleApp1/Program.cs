0b849390519711ddcdc1395ae5c4cee435e256e8
Alok Gautam <gautamalok0001@gmail.com>
2023-05-15 00:57:44 +0530

    initial commit

---
+++ C#/DSAALGO/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppRetro;
+
+Console.WriteLine("Hello, World!");
+SumOfDigitsInFactorialNumber sumOfDigits = new SumOfDigitsInFactorialNumber();
+sumOfDigits.findSumOfDigits(10);
